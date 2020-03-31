860c1dd2bede9cfc625c8ac40adf224c20c6cafe
Brandon Fergerson <fergerson92@gmail.com>
2020-03-31 12:38:18 -0400

    specify kotlin version once

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
+plugins {
+    val kotlinVersion = "1.3.71"
+    kotlin("jvm") version kotlinVersion apply false
+    kotlin("multiplatform") version kotlinVersion apply false
+    kotlin("js") version kotlinVersion apply false
+}
+
 subprojects {
     group = "plus.sourceplus"
     version = "1.0-SNAPSHOT"
